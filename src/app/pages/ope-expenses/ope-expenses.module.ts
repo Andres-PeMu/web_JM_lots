69ccd5f2311cb1a5352e9c6c1274ea8cce81b3c8
Andres Pena <andres.pena.m15@gmail.com>
2023-03-01 20:29:24 -0500

    creadas las paginas

---
+++ src/app/pages/ope-expenses/ope-expenses.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OpeExpensesRoutingModule } from './ope-expenses-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    OpeExpensesRoutingModule
+  ]
+})
+export class OpeExpensesModule { }
