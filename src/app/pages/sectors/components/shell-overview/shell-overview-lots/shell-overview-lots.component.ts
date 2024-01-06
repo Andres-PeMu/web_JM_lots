4ee0b61cae38109b81b20f5fd574f76b9bcb2113
Andres-PeMu <andres.pena.m15@gmail.com>
2024-01-06 02:32:34 -0500

    add error eliminar

--- src/app/pages/sectors/components/shell-overview/shell-overview-lots/shell-overview-lots.component.ts
+++ src/app/pages/sectors/components/shell-overview/shell-overview-lots/shell-overview-lots.component.ts
@@ -4,6 +4,8 @@ import { CardLotsComponent } from '../../cards/card-lots/card-lots.component';
 import { LotsService } from 'src/app/services/Http/lots.service';
 import { DataLotsService } from 'src/app/services/date/data-lots.service';
 import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
+import { ModalService } from 'src/app/services/modal/modal.service';
 
 @Component({
   selector: 'app-shell-overview-lots',
@@ -17,12 +19,27 @@ export class ShellOverviewLotsComponent {
     private _serviceLots: LotsService,
     private dataServiceLots: DataLotsService,
     private router: Router,
-    ){}
+    private messageService: ModalService
+  ) { }
 
-  handleDelete(){
-    this._serviceLots.delete(this.dataServiceLots.id).subscribe(res =>{
-      this.router.navigate(['/sectors'])
-    });
+  handleDelete() {
+    this._serviceLots.delete(this.dataServiceLots.id)
+      .pipe(
+        catchError((error) => {
+          this.messageService.showMessage(
+            `Error en la operación al elimiar ${error.error.message}`,
+            'error'
+          );
+          return [];
+        })
+      )
+      .subscribe(res => {
+        this.messageService.showMessage(
+          'Operación exitosa se ha elimiar',
+          'success'
+        );
+        this.router.navigate(['/sectors'])
+      });
     this._bottomSheetRef.dismiss();
   }
 
